42717cf9530e18e184fdad37e213db5c1fa8104e
Thiago Souza <tsouzatm@outlook.com>
2023-10-11 11:46:40 -0300

    Adicionando o módulo 5

--- FilmesAPI/Profiles/FilmeProfile.cs
+++ FilmesAPI/Profiles/FilmeProfile.cs
@@ -9,6 +9,9 @@ namespace FilmesAPI.Profiles
         public FilmeProfile()
         {
             CreateMap<CreateFilmeDto, Filme>();
+            CreateMap<UpdateFilmeDto, Filme>();
+            CreateMap<Filme, UpdateFilmeDto>();
+            CreateMap<Filme, ReadFilmeDto>();
         }
     }
 }
--- README.md.txt
+++ README.md.txt
@@ -4,3 +4,4 @@ Microsoft.EntityFrameworkCore.Tools 6.0.10
 Pomelo.EntityFrameworkCore.MySql 6.0.2
 AutoMapper 12.0.0
 AutoMapper.Extensions.Microsoft.DependencyInjection 12.0.0
+Microsoft.AspNetCore.Mvc.NewtonsoftJson 6.0.10
